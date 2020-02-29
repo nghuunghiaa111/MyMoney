cea6ecf0364d17ee8bd6d545605ae525499e3f96
Nghia Nguyen Huu <nghuunghiaa@gmail.com>
2020-02-29 17:41:55 +0700

    Initial Commit

---
+++ My Money/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Money
+//
+//  Created by HuuNghia on 2/29/20.
+//  Copyright © 2020 HuuNghia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
